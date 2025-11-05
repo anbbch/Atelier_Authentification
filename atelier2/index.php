804c9e18ed1042802c1c9eccafced98bcbe226c3
anbbch <155988779+anbbch@users.noreply.github.com>
2025-11-05 10:57:31 +0100

    Update index.php

--- atelier2/index.php
+++ atelier2/index.php
@@ -5,7 +5,7 @@ session_start();
 // Vérifier si l'utilisateur est déjà en possession d'un cookie valide (cookie authToken ayant le contenu 12345)
 // Si l'utilisateur possède déjà ce cookie, il sera redirigé automatiquement vers la page home.php
 // Dans le cas contraire il devra s'identifier.
-if (isset($_COOKIE['authToken']) && isset($_SESSION['authToken']) && $_COOKIE['authToken'] === $_SESSION['authToken']) {
+if (isset($_COOKIE['authToken']) || $_COOKIE['authToken'] !== '12345') {
     header('Location: page_admin.php');
     exit();
 }
@@ -20,6 +20,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if ($username === 'admin' && $password === 'secret') {
         setcookie('authToken', bin2hex(random_bytes(16)), time() + 60, '/', '', false, true); // Le Cookie est initialisé et valable pendant 1 heure (3600 secondes) 
         header('Location: page_admin.php'); // L'utilisateur est dirigé vers la page home.php
+        exit();
+    } if ($username === 'user' && $password === 'utilisateur') {
+        setcookie('authToken', bin2hex(random_bytes(16)), time() + 60, '/', '', false, true); // Le Cookie est initialisé et valable pendant 1 heure (3600 secondes) 
+        header('Location: page_user.php'); // L'utilisateur est dirigé vers la page home.php
         exit();
     } else {
         $error = "Nom d'utilisateur ou mot de passe incorrect.";
@@ -37,6 +41,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <body>
     <h1>Atelier authentification par Cookie</h1>
     <h3>La page <a href="page_admin.php">page_admin.php</a> est inaccéssible tant que vous ne vous serez pas connecté avec le login 'admin' et mot de passe 'secret'</h3>
+    <h3>La page <a href="page_user.php">page_user.php</a> est inaccéssible tant que vous ne vous serez pas connecté avec le login 'user' et mot de passe 'utilisateur'</h3>
     <form method="POST" action="">
         <label for="username">Nom d'utilisateur :</label>
         <input type="text" id="username" name="username" required>
