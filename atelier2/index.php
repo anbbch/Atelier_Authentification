8c731c75cd85f594a8445af6586c060d4eba8480
anbbch <155988779+anbbch@users.noreply.github.com>
2025-11-05 11:24:44 +0100

    Update index.php

--- atelier2/index.php
+++ atelier2/index.php
@@ -5,9 +5,15 @@ session_start();
 // Vérifier si l'utilisateur est déjà en possession d'un cookie valide (cookie authToken ayant le contenu 12345)
 // Si l'utilisateur possède déjà ce cookie, il sera redirigé automatiquement vers la page home.php
 // Dans le cas contraire il devra s'identifier.
-if (isset($_COOKIE['authToken']) || $_COOKIE['authToken'] !== '12345') {
-    header('Location: page_admin.php');
-    exit();
+// Vérifie si un cookie valide existe déjà
+if (isset($_COOKIE['authToken'])) {
+    if ($_COOKIE['authToken'] === 'adminToken') {
+        header('Location: page_admin.php');
+        exit();
+    } elseif ($_COOKIE['authToken'] === 'userToken') {
+        header('Location: page_user.php');
+        exit();
+    }
 }
 
 // Gérer la soumission du formulaire
@@ -18,11 +24,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Vérification simple du username et de son password.
     // Si ok alors on initialise le cookie sur le poste de l'utilisateur 
     if ($username === 'admin' && $password === 'secret') {
-        setcookie('authToken', bin2hex(random_bytes(16)), time() + 60, '/', '', false, true); // Le Cookie est initialisé et valable pendant 1 heure (3600 secondes) 
+        setcookie('authToken', 'adminToken', time() + 60, '/', '', false, true); // Le Cookie est initialisé et valable pendant 1 heure (3600 secondes) 
         header('Location: page_admin.php'); // L'utilisateur est dirigé vers la page home.php
         exit();
     } if ($username === 'user' && $password === 'utilisateur') {
-        setcookie('authToken', bin2hex(random_bytes(16)), time() + 60, '/', '', false, true); // Le Cookie est initialisé et valable pendant 1 heure (3600 secondes) 
+        setcookie('authToken', 'userToken', time() + 60, '/', '', false, true); // Le Cookie est initialisé et valable pendant 1 heure (3600 secondes) 
         header('Location: page_user.php'); // L'utilisateur est dirigé vers la page home.php
         exit();
     } else {
